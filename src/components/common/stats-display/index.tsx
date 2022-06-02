a6c2640033f462d4706e33d0f5bd37be73155aa8
Roger_Delacruz <roger.delacruz.jr@gmail.com>
2022-06-02 04:00:48 -0400

    Added stat

--- src/components/common/stats-display/index.tsx
+++ src/components/common/stats-display/index.tsx
@@ -11,6 +11,8 @@ interface StatsDisplayProps {
 }
 
 export const StatsDisplay: FunctionComponent<StatsDisplayProps> = (props) => {
+  const timeElapsed = 60 - props.secondsLeft;
+  const averageWordsPerSecond = timeElapsed ? props.numberOfMatches / timeElapsed : props.numberOfMatches;
   return (
     <div className='stats-display-wrapper'>
       <div className='stat-container'>
@@ -32,6 +34,13 @@ export const StatsDisplay: FunctionComponent<StatsDisplayProps> = (props) => {
           Number of mistakes:
         </span> <span className='mistakes'>
           {props.mistakeCount}
+        </span>
+      </div>
+      <div className='stat-container'>
+        <span className='label'>
+          Average words/sec:
+        </span> <span className='stat'>
+          {averageWordsPerSecond.toFixed(4)}
         </span>
       </div>
     </div>
